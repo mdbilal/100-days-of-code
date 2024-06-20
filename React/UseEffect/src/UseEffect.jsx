dc9a3e03c53cada2ed5c9a5c09e28419d427b50d
mdbilal <mdbilalofficial@gmail.com>
2024-06-20 14:33:49 +0500

    multiple useEffect hooks

--- React/UseEffect/src/UseEffect.jsx
+++ React/UseEffect/src/UseEffect.jsx
@@ -4,21 +4,43 @@ function UseEffect(){
 
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
+    const [count, setCount] = useState(0);
+    const [color, setColor] = useState('black');
 
     useEffect(()=> {
         window.addEventListener("resize", setResize);
     }, []);
+
+    useEffect(()=> {
+        document.title = `Count: ${count} ${color}`;
+    }, [count, color])
 
     const setResize = () => {
         setWidth(window.innerWidth);
         setHeight(window.innerHeight);
     }
 
+    const handleDecremment = () => {
+        setCount(prevCount => prevCount-1);
+    }
+
+    const handleIncrement = () => {
+        setCount(prevCount => prevCount+1);
+    }
+
+    const handleColor = () => {
+        setColor(prevColor => prevColor == 'black' ? 'red' : 'black');
+    }
+
     return (
-        <>
+        <div style={{color: color}}>
             <p>Screen Width: {width} px</p>
             <p>Screen Height: {height} px</p>
-        </>
+            <p>Count: {count}</p>
+            <button onClick={handleDecremment}>-</button>
+            <button onClick={handleIncrement}>+</button>
+            <button onClick={handleColor}>change color</button>
+        </div>
     )
 }
 
